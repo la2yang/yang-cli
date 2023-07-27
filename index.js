d315941eebe464cfa3f3bce27c7d631a2f520131
StaRa1n <1945159551@qq.com>
2023-07-27 21:05:16 +0800

    feat: code

--- index.js
+++ index.js
@@ -1,42 +1,58 @@
-const program = require('commander') // 控制台命令
-const chalk = require('chalk') // 美化工具
-const Inquirer = require('inquirer') // 命令行交互
-const figlet = require('figlet') // 艺术字美化工具
-const { default: Choices } = require('inquirer/lib/objects/choices')
+const program = require('commander')
+const chalk = require('chalk')
+const inquirer = require('inquirer')
+const figlet = require('figlet')
+const ora = require('ora')
+const fs = require('fs-extra')
+const path = require('path')
 
-program.name('yang-cli').usage(`<command>[option]`).version(`1.0.0`)
+// 首行提示
+program.name('yang-cli').usage(`<command> [option]`)
 
-console.log(`${chalk.green.underline.bold('forgive')} me`)
+// 版本号
+program.version(`v${require('./package.json').version}`)
 
-console.log(
-  '\r\n' +
-    figlet.textSync('demo', {
-      font: 'Ghost',
-      horizontalLayout: 'default',
-      verticalLayout: 'default',
-      width: 80,
-      whitespaceBreak: true,
-    })
-)
+// 给help指令添加提示
+program.on('--help', function () {
+  console.log(
+    '\r\n' +
+      figlet.textSync('yang', {
+        font: 'Muzzle',
+        horizontalLayout: 'default',
+        verticalLayout: 'default',
+        width: 80,
+        whitespaceBreak: true,
+      })
+  )
+})
 
-// new Inquirer.prompt([
-//   {
-//     name: 'vue',
-//     type: 'checkbox',
-//     message: 'check the features needed for you project',
-//     choices: [
-//       {
-//         name: 'Babel',
-//         checked: 'true',
-//       },
-//       {
-//         name: 'TypeScript',
-//         checked: 'true',
-//       },
-//     ],
-//   },
-// ]).then((data) => {
-//   console.log(data)
-// })
+// 命令
+// 创建项目的命令
+program
+  .command('create <app-name>')
+  .description('创建一个新项目')
+  .action(async function (name) {
+    // 1.判断当前目录是否有相同命名的文件夹
+    const targetPath = path.join(process.cwd(), name)
+    if (fs.existsSync(targetPath)) {
+      // 存在同名文件
+      const isOverwirte = await inquirer.prompt([
+        {
+          name: 'overwrite',
+          type: 'confirm',
+          message: '已存在同命文件,是否覆盖之前的文件夹',
+          default: false,
+        },
+      ])
+      // 删除原有文件
+      if (isOverwirte) {
+        fs.remove(targetPath)
+        console.log('删除成功')
+      } else {
+        return
+      }
+    }
+    // 2.新建文件(github克隆模板)
+  })
 
 program.parse(program.argv)
--- readme.md
+++ readme.md
@@ -1,1 +1,8 @@
-自用脚手架
+## 自用脚手架
+
+### 相关依赖
+- commander: 命令行处理
+- chalk: 命令行美化
+- figlet: 艺术字
+- inquirer: 命令行交互
+- ora: loading美化
